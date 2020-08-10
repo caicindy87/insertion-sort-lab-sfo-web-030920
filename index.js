950ce48af3e499d82accb99a47caad824cc6f6be
Cindy Cai <caicindy87@gmail.com>
2020-08-10 22:12:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function findMinAndRemove(array){
+  // start with first element. assume it is min for now
+  // minIndex = 0
   
+  // loop through array to check each element
+  // if another elem is less than the current min, reassign min
+  // also reassign minIndex
+  
+  // after finding the min, remove it from the array using splice
+  // return 
 }
 
 function selectionSort(array){
